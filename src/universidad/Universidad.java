65649b56a342da2331b6c214543a8c0b3cfe0977
AriBartellini <aribartellini@gmail.com>
2023-09-11 19:41:52 -0300

    Intento de cargar alumno fallida

--- src/universidad/Universidad.java
+++ src/universidad/Universidad.java
@@ -1,14 +1,20 @@
 package universidad;
 
 
+import java.time.LocalDate;
+import java.time.Month;
+import universidad.acceso.AlumnoData;
 import universidad.acceso.Conexion;
+import universidad.entidades.Alumno;
 
 public class Universidad {
 
     public static void main(String[] args) {
     Conexion con= new Conexion();
-    //comentario 1
-    //comentario ari
+    
+    Alumno ari = new Alumno(40678534,"Bartellini", "Ari", LocalDate.of(1998, Month.JANUARY,8),true);
+    AlumnoData alumno = new AlumnoData();
+    alumno.guardarAlumno(ari);
     }
     
 }
--- src/universidad/acceso/AlumnoData.java
+++ src/universidad/acceso/AlumnoData.java
@@ -6,36 +6,40 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 /*import org.mariadb.jdbc.Statement;*/
 
 public class AlumnoData {
-    private Connection con = null;
+    private final Connection con = null;
+    
     public AlumnoData(){
        
     }
     
     public void guardarAlumno(Alumno alumno){
-        
+        String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNac, estado) VALUES (?,?,?,?,?)";
         try {
-            String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNac, estado) VALUES (?,?,?,?,?)";
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
             ps.setInt(1, alumno.getDni());
             ps.setString(2,alumno.getApellido());
             ps.setString(3, alumno.getNombre());
             ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
             ps.setBoolean(5, alumno.isActivo());
+            
             ps.executeUpdate();
+            
             ResultSet rs= ps.getGeneratedKeys();
             if(rs.next()){
-                
+                alumno.setIdAlumno(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Alumno guardado correctamente");
             }
         
         } catch (SQLException ex) {
-            Logger.getLogger(AlumnoData.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
         }
+        
+        
     }
     
 }
-//hola 

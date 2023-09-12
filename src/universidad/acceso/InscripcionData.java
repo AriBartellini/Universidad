ee22ec0f751efa986a0fd20c8b65efb0f2b76c71
MauriMateos <mauriciomicaelmateos@gmail.com>
2023-09-12 20:15:24 -0300

    update inscripcion data

--- src/universidad/acceso/InscripcionData.java
+++ src/universidad/acceso/InscripcionData.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universidad.acceso;
 
-/**
- *
- * @author Ari
- */
+
 public class InscripcionData {
-    //hola este es un comentario
-    //aaaaaaaaa
+
+    public InscripcionData() {
+    }
+   
+    
 }
  

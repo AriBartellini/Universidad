2aa3616665cba0e8c77b51898e80b9714fcc53c9
AriBartellini <aribartellini@gmail.com>
2023-09-15 18:32:20 -0300

    Update InscripcionData.java

--- src/universidad/acceso/InscripcionData.java
+++ src/universidad/acceso/InscripcionData.java
@@ -1,6 +1,12 @@
 package universidad.acceso;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+import universidad.entidades.Inscripcion;
 
 
 public class InscripcionData {
@@ -15,8 +21,16 @@ public class InscripcionData {
    
     public void guardarInscripcion(Inscripcion ins){
         String sql = "INSERT INTO inscripcion (idAlumno, idMateria) VALUES (?,?)";
-        
-    }
+        try {   
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1,ins.getIdInscripcion());
+            ps.setInt(2, ins.getMateria());
+            
+            
+        } catch (SQLException ex) {
+         JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");
+        }
     
+    }
 }
  

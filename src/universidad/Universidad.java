7964df8f078b84e77525db3934b4d283671d52f0
AriBartellini <aribartellini@gmail.com>
2023-09-23 16:41:41 -0300

    Update Universidad.java

--- src/universidad/Universidad.java
+++ src/universidad/Universidad.java
@@ -1,12 +1,13 @@
 package universidad;
 
+import javax.swing.UnsupportedLookAndFeelException;
 import universidad.acceso.Conexion;
 import universidad.vistas.Uni;
 
 
 public class Universidad {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
     Conexion con= new Conexion();
     
     Uni.main(args);

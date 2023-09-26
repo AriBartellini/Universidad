5b83f04f5b33572dfbd89a552c7669aaba08631f
MauriMateos <mauriciomicaelmateos@gmail.com>
2023-09-26 20:38:20 -0300

    Update InscripcionData.java

--- src/universidad/acceso/InscripcionData.java
+++ src/universidad/acceso/InscripcionData.java
@@ -26,27 +26,7 @@ public class InscripcionData {
         con = Conexion.buscarConexion();
     }
 
-    public void guardarInscripcion(Inscripcion ins) {
-        String sql = "INSERT INTO inscripcion (idAlumno, idMateria, nota) VALUES (?,?,?)";
     
-        try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, ins.getAlumno().getIdAlumno());
-            ps.setInt(2, ins.getMateria().getIdMateria());
-            ps.setDouble(3, ins.getNota());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                
-                ins.setIdInscripcion(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Inscripcion cargada correctamente");
-            }
-            ps.close();
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion");
-        }
-    }
     
     public void  guardarInscripcion(int idAlumno, int idMateria) {
         String sql = "INSERT INTO inscripcion (idAlumno, idMateria, nota) VALUES (?,?,?)";
